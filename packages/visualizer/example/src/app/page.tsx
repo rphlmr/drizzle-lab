f4691b24a863ec5e6b185d016123f9cbebcddeb9
rphlmr <raphael@uaerom.com>
2025-01-25 19:22:41 +0100

    up

--- packages/visualizer/example/src/app/page.tsx
+++ packages/visualizer/example/src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start size-full">
-        <h1>Drizzle Visualizer</h1>
+        <h1 className="text-2xl font-bold">
+          Drizzle Visualizer in a Next.js App
+        </h1>
         <div className="w-full h-full">
           <ClientOnly fallback={<div>Loading...</div>}>
             <Visualizer />
