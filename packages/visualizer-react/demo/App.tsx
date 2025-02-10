5f7f9a1461ffbe75401ccfcb810914d3334b1060
rphlmr <raphael@uaerom.com>
2025-02-10 07:27:59 +0100

    wip

---
+++ packages/visualizer-react/demo/App.tsx
@@ -0,0 +1,14 @@
+import { Visualizer } from "../src";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Visualizer data={{}} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/visualizer-react/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import dts from "vite-plugin-dts";
+import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({
+      include: ["src"],
+      tsconfigPath: "./tsconfig.json",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      // the proper extensions will be added
+      fileName: "index",
+      formats: ["es"],
+      cssFileName: "style",
+    },
+    copyPublicDir: false,
+    minify: false,
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+});
